1024fde4f8e3a792f53cb32c4b5a55f0f9b012cc
wbin <33045209+wbin327@users.noreply.github.com>
2019-08-14 18:30:42 +0800

    修改：接入微信分享接口

--- static/js/index-phone.js
+++ static/js/index-phone.js
@@ -27,3 +27,50 @@ $(".content-group").click(function () {
     let url = `${baseUrl}/details-page-phone.html?index=${index}`;
     window.location.href = url;
 })
+
+// 微信分享
+function wechatShare(){
+    const wx = window['wx']
+    const url = location.href.split('#')[0];
+    $.get("114.215.188.153:5000/getWeChatToken?url=" + encodeURIComponent(url), function (res) {
+        if(res.code === 200){
+            wx.config({
+                debug: true,
+                appId: res.data.appId, // 必填，公众号的唯一标识
+                timestamp: res.data.timestamp, // 必填，生成签名的时间戳
+                nonceStr: res.data.nonceStr, // 必填，生成签名的随机串
+                signature: res.data.signature, // 必填，签名
+                jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareQZone', 'onMenuShareWeibo'] // 必填，需要使用的JS接口列表
+            });
+
+            wx.ready(function () {
+                const title = '宏易资本集团';
+                const desc = '宏易资本集团';
+                const imgUrl = `${baseUrl}/static/images/share.png`;
+
+                // 分享给好友
+                wx.updateAppMessageShareData({
+                    title: title, // 分享标题
+                    desc: desc, // 分享描述
+                    link: location.href.split('#')[0], // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                    imgUrl: imgUrl, // 分享图标
+                    success: function () {
+                        // 设置成功
+                        console.log("分享给好友成功")
+                    }
+                })
+                // 分享到朋友圈
+                wx.updateTimelineShareData({
+                    title: title, // 分享标题
+                    link: desc, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                    link: location.href.split('#')[0],
+                    imgUrl: imgUrl, // 分享图标
+                    success: function () {
+                        // 设置成功
+                        console.log("分享到朋友圈成功")
+                    }
+                })
+            })
+        }
+    })
+}
